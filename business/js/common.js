5be9d3676996ef056913e57198a5c962d649970e
markuslutrell <39496379+markuslutrell@users.noreply.github.com>
2019-03-02 16:01:02 +0300

    Add files via upload

---
+++ business/js/common.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $preloader = $('.preloader'),
+    $loader = $preloader.find('.preloader');
+    $loader.fadeOut();
+    $preloader.delay(450).fadeOut(500);
+});
+
+
+// Модальное окно
+
+// открыть по кнопке
+$('.js-button-campaign').click(function() { 
+  $('.js-overlay-campaign').fadeIn(250);
+  $('.js-overlay-campaign').addClass('disabled');
+});
+
+
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+  $('.js-overlay-campaign').fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+  var popup = $('.js-popup-campaign');
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.js-overlay-campaign').fadeOut();
+  }
+});
+
+
+$('.feedback__slider').slick();
+
+$(".tab_item").not(":first").hide();
+$(".wrapper .tab").click(function() {
+  $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+  $(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
