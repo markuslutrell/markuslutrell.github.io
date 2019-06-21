83167e79606baada5d2d7a58c56aae8b62819f7d
markuslutrell <39496379+markuslutrell@users.noreply.github.com>
2019-06-22 00:50:27 +0300

    Add files via upload

--- pill3/js/common.js
+++ pill3/js/common.js
@@ -23,7 +23,29 @@ $( function() {
   
 });
 
-$(document).ready(function() { 
-  $("section").animated("fadeIn", 2000);
-  $(".feedback-wrapper").animated("fadeIn", 2000);  
+function startTimer(duration, display) {
+    var timer = duration, hours, minutes, seconds;
+    setInterval(function () {
+        hours = parseInt(timer / 3600, 0);
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(hours + ' : ' + minutes + " : " +  seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var fiveMinutes = 60 * 10, //Здесь меняется время таймера
+        display = $('.time');
+    startTimer(fiveMinutes, display);
 });
