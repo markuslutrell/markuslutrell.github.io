715c6593359bc2c1fe6832afe0d1f2a6b56a7341
markuslutrell <39496379+markuslutrell@users.noreply.github.com>
2019-08-17 17:37:16 +0300

    Add files via upload

--- china/js/common.js
+++ china/js/common.js
@@ -236,3 +236,7 @@ $(document).mouseup(function (e) {
   }
 });
 
+$(".accordeon dd").hide().prev().click(function() {
+    $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
+    $(this).next().not(":visible").slideDown().prev().addClass("active");
+});
