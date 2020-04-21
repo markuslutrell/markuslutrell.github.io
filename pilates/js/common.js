dd0982c415639efd89ba63d9fe823e8b4dbb7aed
Roman Kyktenko <39496379+romankyktenko@users.noreply.github.com>
2020-04-21 11:27:45 +0300

    Add files via upload

---
+++ pilates/js/common.js
@@ -0,0 +1,130 @@
+window.onload = function () {
+    document.body.classList.add('loaded');
+}
+
+
+$( function() {
+  //On Scroll Functionality
+  $(window).scroll( function() {
+    var windowTop = $(window).scrollTop();
+    windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+    windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+  });
+  
+  //Smooth Scrolling Using Navigation Menu
+  $('a[href*="#"]').on('click', function(e){
+    $('html,body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    },500);
+    e.preventDefault();
+  });
+
+  
+}); 
+
+
+
+$(document).ready(function() {
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.js-overlay-campaign4').fadeIn();
+            $('.js-overlay-campaign4').addClass('disabled');
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+});
+
+
+
+// Модальное окно
+
+// открыть по кнопке
+$('.js-button-campaign').click(function() {  
+  $('.js-overlay-campaign').fadeIn();
+  $('.js-overlay-campaign').addClass('disabled');
+});
+
+$('.js-button-campaign2').click(function() {  
+  $('.js-overlay-campaign2').fadeIn();
+  $('.js-overlay-campaign2').addClass('disabled');
+});
+
+$('.js-button-campaig3').click(function() { 
+  $('.js-overlay-campaign3').fadeIn();
+  $('.js-overlay-campaign3').addClass('disabled');
+});
+
+$('.js-button-campaign5').click(function() { 
+  $('.js-overlay-campaign5').fadeIn();
+  $('.js-overlay-campaign5').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+  $('.js-overlay-campaign').fadeOut();
+  $('.js-overlay-campaign2').fadeOut();
+  $('.js-overlay-campaign3').fadeOut();
+  $('.js-overlay-campaign4').fadeOut();
+  $('.js-overlay-campaign5').fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+  var popup = $('.js-popup-campaign');
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.js-overlay-campaign').fadeOut();  }
+});
+
+$(document).mouseup(function (e) { 
+  var popup = $('.js-popup-campaign2');
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.js-overlay-campaign2').fadeOut();
+  }
+});
+
+$(document).mouseup(function (e) { 
+  var popup = $('.js-popup-campaign3');
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.js-overlay-campaign3').fadeOut();
+  }
+});
+
+$(".accordeon dd").hide().prev().click(function() {
+  $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
+  $(this).next().not(":visible").slideDown().prev().addClass("active");
+});
+
+
+
+if ($(window).width() < 760) {
+    $('.why__items').slick({
+      dots: false,
+      infinite: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      speed: 1000
+    });
+  }
+
+$('.what-is__slider').slick({
+  centerMode: true,
+  dots: false,
+  infinite: true,
+  arrows: false,
+  variableWidth: true,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  speed: 1000
+});
